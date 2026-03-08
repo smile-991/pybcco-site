610b08bf2af8fe82848a05deb61e51205fd7dcb3
smile-991 <m.h.jabasini@gmail.com>
2026-03-08 07:22:58 +0300

    Update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -73,7 +73,7 @@ import DifferenceBetweenShellAndFinishing from "@/engineering-insights/construct
 import VillaConstructionExecutionSchedule from "@/engineering-insights/construction-and-finishing-stages/VillaConstructionExecutionSchedule";
 import HowToMonitorFinishingQuality from "@/engineering-insights/construction-and-finishing-stages/HowToMonitorFinishingQuality";
 import ConstructionAndFinishingStagesPage from "@/engineering-insights/construction-and-finishing-stages";
-import CommonMistakesPage from "./engineering-insights/common-mistakes";
+import CommonMistakesPage from "@/engineering-insights/common-mistakes";
 
 export default function App() {
   return (
